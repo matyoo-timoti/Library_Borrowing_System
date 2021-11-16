e1bb7cfd57aa71a1b5ffdd71b31bb39767fc8f0f
matyoo-timoti <timsmothy@gmail.com>
2021-11-16 21:45:51 +0800

    • 2nd Commit. • Saves the file just fine. • Still has issue regarding the notification pop-up.

--- src/com/lms/gui/AddNewEntry.java
+++ src/com/lms/gui/AddNewEntry.java
@@ -10,21 +10,57 @@ class AddNewEntry {
     private static final JButton SAVE_BUTTON = new JButton("Save");
     private static final Color CADET_BLUE = new Color(88, 164, 176);
     private static final JPanel popup_container = new JPanel();
+    private static final JLabel popup_label = new JLabel("Entry Saved!");
+
+    //Title of the Book  
+    private static final JLabel book_title_label = new JLabel("Title of the Book:");
+    private static final JPanel book_title_cont = new JPanel();
+    private static final JTextField book_title_tf = new JTextField();
+
+    //Author &  Date Published  
+    private static final JLabel author_date_label = new JLabel("Author & Year Published:");
+    private static final JTextField author_date_tf = new JTextField();
+    private static final JPanel author_date_cont = new JPanel();
+
+    //ISBN  
+    private static final JLabel isbn_label = new JLabel("ISBN:");
+    private static final JTextField isbn_tf = new JTextField();
+    private static final JPanel isbn_cont = new JPanel();
+
+    //Date Borrowed  
+    private static final JLabel date_borrowed_label = new JLabel("Date Borrowed (mm-dd-yyyy):");
+    private static final JTextField date_borrowed_tf = new JTextField();
+    private static final JPanel date_borrowed_cont = new JPanel();
+
+    //Name of Borrower  
+    private static final JLabel borrower_name_label = new JLabel("Borrower Name:");
+    private static final JTextField borrower_name_tf = new JTextField();
+    private static final JPanel borrower_name_cont = new JPanel(new GridLayout(2, 1));
+
+    //Affiliation  
+    private static final JLabel affiliation_label = new JLabel("Department/Course/Section:");
+    private static final JTextField affiliation_tf = new JTextField();
+    private static final JPanel affiliation_cont = new JPanel(new GridLayout(2, 1));
+
+    //Due Date  
+    private static final JLabel due_date_label = new JLabel("Due Date (mm-dd-yyyy):");
+    private static final JTextField due_date_tf = new JTextField();
+    private static final JPanel due_date_cont = new JPanel(new GridLayout(2, 1));
+
+    static FileHandling file_handling = new FileHandling();
 
     public static void main(String[] args) {
         WINDOW.setSize((int) (get_screen_width() / 2.7), (int) (get_screen_height() / 2.5));
-        //<--------------------Pop-up Notification-------------------->
-        JLabel popup_label = new JLabel("Entry Saved!");
+        //Pop-up Notification   
         popup_label.setFont(new Font("Inter", Font.BOLD, 20));
         popup_label.setForeground(Color.WHITE);
-
         popup_container.setSize(200, 40);
         popup_container.setBackground(new Color(148, 210, 189));
         popup_container.setBorder(BorderFactory.createLineBorder(new Color(94, 187, 156), 1, true));
         popup_container.add(popup_label);
         popup_container.setVisible(false); //hidden at first
 
-        //<--------------------Title Container-------------------->
+        //Title Container  
         JLabel title = new JLabel("Add New Entry", JLabel.LEFT);
         title.setFont(new Font("Inter", Font.BOLD, 30));
         title.setForeground(Color.WHITE);
@@ -36,125 +72,28 @@ class AddNewEntry {
         title_panel.add(title, BorderLayout.LINE_START);
         title_panel.add(popup_container, BorderLayout.EAST);
 
-        //<--------------------Left Side Container-------------------->
+        //Left Side Container  
         JPanel left_cont = new JPanel();
         left_cont.setLayout(new GridLayout(4, 1));
         left_cont.setBackground(Color.white);
         left_cont.setSize(100, -1);
         left_cont.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 10));
+        left_cont.add(panel_format(book_title_cont, book_title_label, book_title_tf));
+        left_cont.add(panel_format(author_date_cont, author_date_label, author_date_tf));
+        left_cont.add(panel_format(isbn_cont, isbn_label, isbn_tf));
+        left_cont.add(panel_format(date_borrowed_cont, date_borrowed_label, date_borrowed_tf));
 
-        //<--------------------Title of the Book-------------------->
-        JLabel book_title_label = new JLabel("Title of the Book:");
-        book_title_label.setForeground(Color.black);
-        book_title_label.setFont(new Font("Inter Medium", Font.PLAIN, 15));
-
-        JTextField book_title_tf = new JTextField();
-        book_title_tf.setFont(new Font("Inter Medium", Font.PLAIN, 15));
-        book_title_tf.setBackground(new Color(248, 248, 248));
-
-        JPanel book_title_cont = new JPanel(new GridLayout(2, 1));
-        book_title_cont.setBackground(Color.white);
-        book_title_cont.add(book_title_label);
-        book_title_cont.add(book_title_tf);
-        left_cont.add(book_title_cont);
-
-        //<--------------------Author &  Date Published-------------------->
-        JLabel author_date_label = new JLabel("Author & Year Published:");
-        author_date_label.setForeground(Color.black);
-        author_date_label.setFont(new Font("Inter Medium", Font.PLAIN, 15));
-
-        JTextField author_date_tf = new JTextField();
-        author_date_tf.setFont(new Font("Inter Medium", Font.PLAIN, 15));
-        author_date_tf.setBackground(new Color(248, 248, 248));
-
-        JPanel author_date_cont = new JPanel(new GridLayout(2, 1));
-        author_date_cont.setBackground(Color.white);
-        author_date_cont.add(author_date_label);
-        author_date_cont.add(author_date_tf);
-        left_cont.add(author_date_cont);
-
-        //<--------------------ISBN-------------------->
-        JLabel isbn_label = new JLabel("ISBN:");
-        isbn_label.setForeground(Color.black);
-        isbn_label.setFont(new Font("Inter Medium", Font.PLAIN, 15));
-
-        JTextField isbn_tf = new JTextField();
-        isbn_tf.setFont(new Font("Inter Medium", Font.PLAIN, 15));
-        isbn_tf.setBackground(new Color(248, 248, 248));
-
-        JPanel isbn_cont = new JPanel(new GridLayout(2, 1));
-        isbn_cont.setBackground(Color.white);
-        isbn_cont.add(isbn_label);
-        isbn_cont.add(isbn_tf);
-        left_cont.add(isbn_cont);
-
-        //<--------------------Date Borrowed-------------------->
-        JLabel date_borrowed_label = new JLabel("Date Borrowed (mm-dd-yyyy):");
-        date_borrowed_label.setForeground(Color.black);
-        date_borrowed_label.setFont(new Font("Inter Medium", Font.PLAIN, 15));
-
-        JTextField date_borrowed_tf = new JTextField();
-        date_borrowed_tf.setFont(new Font("Inter Medium", Font.PLAIN, 15));
-        date_borrowed_tf.setBackground(new Color(248, 248, 248));
-
-        JPanel date_borrowed_cont = new JPanel(new GridLayout(2, 1));
-        date_borrowed_cont.setBackground(Color.white);
-        date_borrowed_cont.add(date_borrowed_label);
-        date_borrowed_cont.add(date_borrowed_tf);
-        left_cont.add(date_borrowed_cont);
-
-        //<--------------------Right Side Container-------------------->
+        //Right Side Container  
         JPanel right_cont = new JPanel(new GridLayout(4, 1));
         right_cont.setBackground(Color.white);
         right_cont.setSize(100, -1);
         right_cont.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 20));
 
-        //<--------------------Name of Borrower-------------------->
-        JLabel borrower_name_label = new JLabel("Borrower Name:");
-        borrower_name_label.setForeground(Color.black);
-        borrower_name_label.setFont(new Font("Inter Medium", Font.PLAIN, 15));
+        right_cont.add(panel_format(borrower_name_cont, borrower_name_label, borrower_name_tf));
+        right_cont.add(panel_format(affiliation_cont, affiliation_label, affiliation_tf));
+        right_cont.add(panel_format(due_date_cont, due_date_label, due_date_tf));
 
-        JTextField borrower_name_tf = new JTextField();
-        borrower_name_tf.setFont(new Font("Inter Medium", Font.PLAIN, 15));
-        borrower_name_tf.setBackground(new Color(248, 248, 248));
-
-        JPanel borrower_name_cont = new JPanel(new GridLayout(2, 1));
-        borrower_name_cont.setBackground(Color.white);
-        borrower_name_cont.add(borrower_name_label);
-        borrower_name_cont.add(borrower_name_tf);
-        right_cont.add(borrower_name_cont);
-
-        //<--------------------Affiliation-------------------->
-        JLabel affiliation_label = new JLabel("Department/Course/Section:");
-        affiliation_label.setForeground(Color.black);
-        affiliation_label.setFont(new Font("Inter Medium", Font.PLAIN, 15));
-
-        JTextField affiliation_tf = new JTextField();
-        affiliation_tf.setFont(new Font("Inter Medium", Font.PLAIN, 15));
-        affiliation_tf.setBackground(new Color(248, 248, 248));
-
-        JPanel affiliation_cont = new JPanel(new GridLayout(2, 1));
-        affiliation_cont.setBackground(Color.white);
-        affiliation_cont.add(affiliation_label);
-        affiliation_cont.add(affiliation_tf);
-        right_cont.add(affiliation_cont);
-
-        //<--------------------Due Date-------------------->
-        JLabel due_date_label = new JLabel("Due Date (mm-dd-yyyy):");
-        due_date_label.setForeground(Color.black);
-        due_date_label.setFont(new Font("Inter Medium", Font.PLAIN, 15));
-
-        JTextField due_date_tf = new JTextField();
-        due_date_tf.setFont(new Font("Inter Medium", Font.PLAIN, 15));
-        due_date_tf.setBackground(new Color(248, 248, 248));
-
-        JPanel due_date_cont = new JPanel(new GridLayout(2, 1));
-        due_date_cont.setBackground(Color.white);
-        due_date_cont.add(due_date_label);
-        due_date_cont.add(due_date_tf);
-        right_cont.add(due_date_cont);
-
-        //<--------------------Save Button-------------------->
+        //Save Button  
         JPanel btn_cont = new JPanel(new BorderLayout());
         btn_cont.setBackground(Color.white);
         btn_cont.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
@@ -164,7 +103,7 @@ class AddNewEntry {
         btn_cont.add(SAVE_BUTTON, BorderLayout.CENTER);
         right_cont.add(btn_cont);
 
-        //<--------------------Inner Container-------------------->
+        //Inner Container  
         JPanel container_panel = new JPanel();
         container_panel.setLayout(new GridLayout(0, 2));
         container_panel.setBorder(BorderFactory.createEmptyBorder(0, 0, 20, 0));
@@ -172,13 +111,13 @@ class AddNewEntry {
         container_panel.add(left_cont);
         container_panel.add(right_cont);
 
-        //<--------------------Main Container-------------------->
+        //Main Container  
         JPanel main_panel = new JPanel();
         main_panel.setLayout(new GridLayout());
         main_panel.setOpaque(true);
         main_panel.setBackground(new Color(255, 255, 255, 10));
 
-        //<--------------------Window Configuration-------------------->
+        //Window Configuration  
         WINDOW.setContentPane(main_panel);
         WINDOW.setLayout(new BorderLayout());
         WINDOW.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -188,34 +127,68 @@ class AddNewEntry {
         WINDOW.add(container_panel, BorderLayout.CENTER);
         WINDOW.setResizable(false);
 
-        //<--------------------Button Action-------------------->
-            SAVE_BUTTON.addActionListener(e -> {
-                String bt = book_title_tf.getText().trim();
-                String ad = author_date_tf.getText().trim();
-                long is = Long.parseLong(isbn_tf.getText().trim());
-                String bn = borrower_name_tf.getText().trim();
-                String af = affiliation_tf.getText().trim();
-                String db = date_borrowed_tf.getText().trim();
-                String dd = due_date_tf.getText().trim();
-                FileHandling.Save(bt, ad, is, bn, af, db, dd);
-//                book_title_tf.setText(null);
-//                author_date_tf.setText(null);
-//                isbn_tf.setText(null);
-//                borrower_name_tf.setText(null);
-//                affiliation_tf.setText(null);
-//                date_borrowed_tf.setText(null);
-//                due_date_tf.setText(null);
-//                popup_container.setVisible(true);
-//                delay();
-//                popup_container.setVisible(false);
-                WINDOW.dispose();
-            });
+        //Button Action
+        popup_container.setVisible(false);
+        SAVE_BUTTON.addActionListener(e -> {
+            var bt = get_text(book_title_tf);
+            var ad = get_text(author_date_tf);
+            var is = get_text(isbn_tf);
+            var bn = get_text(borrower_name_tf);
+            var af = get_text(affiliation_tf);
+            var db = get_text(date_borrowed_tf);
+            var dd = get_text(due_date_tf);
+
+            if (form_incomplete()) {
+                JOptionPane.showMessageDialog(WINDOW, "Fields can't be empty!", "Error", JOptionPane.ERROR_MESSAGE);
+            } else {
+                file_handling.save(bt, ad, is, bn, af, db, dd);
+                book_title_tf.setText("");
+                author_date_tf.setText("");
+                isbn_tf.setText("");
+                borrower_name_tf.setText("");
+                affiliation_tf.setText("");
+                date_borrowed_tf.setText("");
+                due_date_tf.setText("");
+            }
+            popup_container.setVisible(true);
+        });
+        delay(1500);
     }
 
-    private static void is_empty(JTextField tf) {
-        if (tf.getText().isEmpty()) {
-            tf.setBorder(new LineBorder(Color.red, 1));
-        }
+    private static boolean form_incomplete() {
+        var bt = get_text(book_title_tf);
+        var ad = get_text(author_date_tf);
+        var is = get_text(isbn_tf);
+        var bn = get_text(borrower_name_tf);
+        var af = get_text(affiliation_tf);
+        var db = get_text(date_borrowed_tf);
+        var dd = get_text(due_date_tf);
+        return bt.isEmpty() || ad.isEmpty() || is.isEmpty() || bn.isEmpty() || af.isEmpty() || db.isEmpty() || dd.isEmpty();
+    }
+
+    private static JPanel panel_format(JPanel panel, JLabel label, JTextField field) {
+        panel.setLayout(new GridLayout(2, 1));
+        panel.setBackground(Color.white);
+        panel.add(label_format(label));
+        panel.add(field_format(field));
+        return panel;
+    }
+
+    private static JLabel label_format(JLabel label) {
+        label.setForeground(Color.black);
+        label.setFont(new Font("Inter Medium", Font.PLAIN, 15));
+        return label;
+    }
+
+    private static JTextField field_format(JTextField tf) {
+        tf.setFont(new Font("Inter Medium", Font.PLAIN, 15));
+        tf.setBackground(new Color(248, 248, 248));
+        tf.setBorder(new LineBorder(new Color(225, 225, 225)));
+        return tf;
+    }
+
+    private static String get_text(JTextField tf) {
+        return tf.getText().trim();
     }
 
     private static double get_screen_width() {
@@ -226,9 +199,9 @@ class AddNewEntry {
         return Toolkit.getDefaultToolkit().getScreenSize().getHeight();
     }
 
-    private static void delay() {
+    private static void delay(int millis) {
         try {
-            Thread.sleep(2000);
+            Thread.sleep(millis);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/com/lms/gui/FileHandling.java
+++ src/com/lms/gui/FileHandling.java
@@ -4,12 +4,12 @@ import javax.swing.*;
 import java.io.*;
 
 public class FileHandling {
-    public static void Save(String bt, String ad, long is, String bn, String af, String db, String dd) {
+
+    private static final String path = System.getProperty("user.home") + File.separator + "Documents" + File.separator + "Library Borrowing System";
+
+    public void save(String book_title, String author_date, String ISBN, String borrower_name, String affiliation, String date_borrowed, String due_date) {
         StringBuilder id = new StringBuilder();
-        id.append(bt.charAt(0));
-        id.append(bn.charAt(0));
-        id.append("-").append(db).append("-").append(dd);
-        String path = System.getProperty("user.home") + File.separator + "Documents" + File.separator + "Library Borrowing System";
+        id.append(book_title).append("_").append(borrower_name).append("_").append(date_borrowed).append("_").append(due_date);
         File folder = new File(path);
         if (folder.exists() || folder.mkdirs()) {
             File new_file = new File(path + File.separator + id + ".txt");
@@ -18,20 +18,21 @@ public class FileHandling {
                     System.out.println("File created: " + new_file.getName());
                     try {
                         FileWriter file_writer = new FileWriter(path + File.separator + id + ".txt");
-                        file_writer.write(bt + "\n");
-                        file_writer.write(ad + "\n");
-                        file_writer.write(is + "\n");
-                        file_writer.write(bn + "\n");
-                        file_writer.write(af + "\n");
-                        file_writer.write(db + "\n");
-                        file_writer.write(dd);
+                        file_writer.write(book_title + "\n");
+                        file_writer.write(author_date + "\n");
+                        file_writer.write(ISBN + "\n");
+                        file_writer.write(borrower_name + "\n");
+                        file_writer.write(affiliation + "\n");
+                        file_writer.write(date_borrowed + "\n");
+                        file_writer.write(due_date);
                         file_writer.close();
                     } catch (IOException e) {
                         JOptionPane.showMessageDialog(null, "An Error Occurred");
                         e.printStackTrace();
                     }
                 } else {
-                    System.out.println("File already exists.");
+//                    JOptionPane.showMessageDialog(x, "There are similar entry in the system.", "Error", JOptionPane.ERROR_MESSAGE);
+                    System.out.println("File Already Exists!");
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -39,5 +40,29 @@ public class FileHandling {
         } else {
             System.out.println("Not created");
         }
+    }
+
+    public String[] retrieve(String id) {
+        String[] entry = new String[7];
+        try {
+            FileReader file_reader = new FileReader(path + File.separator + id + ".txt");
+            BufferedReader buff_reader = new BufferedReader(file_reader);
+            for (int line = 0; line < 7; line++) {
+                switch (line) {
+                    case 0 -> entry[0] = buff_reader.readLine(); //Book title;
+                    case 1 -> entry[1] = buff_reader.readLine(); //Author and year published
+                    case 2 -> entry[2] = buff_reader.readLine(); //ISBN
+                    case 3 -> entry[3] = buff_reader.readLine(); //Borrower name
+                    case 4 -> entry[4] = buff_reader.readLine(); //Borrower affiliation
+                    case 5 -> entry[5] = buff_reader.readLine(); //Date Borrowed
+                    case 6 -> entry[6] = buff_reader.readLine(); //Due date
+                    default -> buff_reader.readLine();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "File not found!", "Warning", JOptionPane.WARNING_MESSAGE);
+        }
+        return entry;
     }
 }
